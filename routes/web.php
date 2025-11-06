4df337d427cfaa982e5b1cc11796d78e9772fca6
emzhran <zachran750@gmail.com>
2025-11-06 16:14:27 +0700

    tambah route ke halaman admin

--- routes/web.php
+++ routes/web.php
@@ -8,30 +8,55 @@ use App\Http\Controllers\VoliController;
 use App\Http\Controllers\BasketController;
 use App\Http\Controllers\JadwalController;
 use App\Http\Controllers\RiwayatController;
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\AdminLapanganController;
 
 Route::get('/', function () {
     return redirect()->route('login');
 })->name('home');
 
-// Route::get('/login', function () {
-//     return view('login');
-// })->name('login');
 
 Route::middleware('auth')->group(function () {
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
 
-    // Dashboard
+        Route::prefix('lapangan')->name('lapangan.')->group(function () {
+            Route::get('/futsal', [AdminLapanganController::class, 'futsalIndex'])->name('futsal');
+            Route::patch('/{type}/{id}/status/{status}', [AdminLapanganController::class, 'updateStatus'])->name('update_status');
+            Route::get('/badminton', function () {
+                return view('admin.lapangan.badminton');
+            })->name('badminton');
+            Route::get('/voli', function () {
+                return view('admin.lapangan.voli');
+            })->name('voli');
+            Route::get('/basket', function () {
+                return view('admin.lapangan.basket');
+            })->name('basket');
+            Route::get('/create', function () {
+                return view('admin.lapangan.create');
+            })->name('create');
+        });
+
+        Route::get('/jadwal', function () {
+            return view('admin.jadwal.index');
+        })->name('jadwal.index');
+
+        Route::get('/riwayat', function () {
+            return view('admin.riwayat.index');
+        })->name('riwayat.index');
+
+        Route::get('/users', function () {
+            return view('admin.users.index');
+        })->name('users.index');
+    });
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 
-    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/jadwal', function () {
-        return view('jadwal.index');
-    })->name('jadwal.index');
 
     Route::get('/booking', function () {
         return view('booking.booking');
@@ -51,9 +76,6 @@ Route::middleware('auth')->group(function () {
         return view('lapangan.voli');
     })->name('voli');
     Route::post('/voli/store', [VoliController::class, 'store'])->name('voli.store');
-    Route::get('/jadwal', function () {
-        return view('lapangan.jadwal-lapangan');
-    })->name('jadwal.index');
 
     Route::get('/basket', function () {
         return view('lapangan.basket');
@@ -61,10 +83,6 @@ Route::middleware('auth')->group(function () {
     Route::post('/basket/store', [BasketController::class, 'store'])->name('basket.store');
 
     Route::get('/jadwal', [JadwalController::class, 'index'])->name('jadwal.index');
-
-    Route::get('/riwayat', function () {
-        return view('riwayat.index');
-    })->name('riwayat.index');
 
     Route::get('/riwayat', [RiwayatController::class, 'index'])->name('riwayat.index');
     Route::post('/riwayat/store', [RiwayatController::class, 'store'])->name('riwayat.store');
